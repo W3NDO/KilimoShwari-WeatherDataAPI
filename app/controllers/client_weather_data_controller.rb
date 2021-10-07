ad2545dcd7c1d78fe3a565db43156db2bf091de4
W3NDO <wendonynag@gmail.com>
2021-10-07 17:57:46 +0300

    Keys working, weather data being collected. Need to automate data collection every 24 hours

--- app/controllers/client_weather_data_controller.rb
+++ app/controllers/client_weather_data_controller.rb
@@ -3,30 +3,32 @@ class ClientWeatherDataController < ApplicationController
     
     def create
         client = ClientWeatherDatum.new(client_params)
+        
         if client.valid?
             client.save
             render json: {status: "SUCCESS", message: "new client created", data: client}, status: :ok
         else
             render json: {status: "FAILURE", message: "Failed to Create New Client", data: client.errors}, status: :unprocessable_entity
         end
+    
     end
 
     def update
         #run the update everyday. 
         client = ClientWeatherDatum.find_by(name: params[:name])
-        weather = {}
+        weather = []
         latitude = client.geo_location.split(',')[0]
         longitude = client.geo_location.split(',')[1]
         res = get_weather(latitude, longitude) #call weather API & input data in to array
         if res[0] 
-            weather << {Date.today => res} 
-            client.weather_data << weather
+            weather << {Date.today => res[1].to_s} 
+            client.weather_data = client.weather_data + "," + weather.to_s
             if client.valid?
                 client.save
-                render json: {status: "SUCCESS", message: "Got Weather for #{Date.today}", data: client}, status: :ok
+                render json: {status: "SUCCESS", message: "Got Weather for #{Date.today}, :: #{res[2]}", data: client}, status: :ok
             end
         else
-            render json: {status: "FAILURE", message: "Failed to get weather for #{Date.today} && #{res[1]}"}, status: :unprocessable_entity
+            render json: {status: "FAILURE", message: "Failed to get weather for #{Date.today} && Error Code from OpenWeather: #{res[1]}, , :: #{res[2]}"}, status: :unprocessable_entity
         end
     end
 
@@ -38,10 +40,10 @@ class ClientWeatherDataController < ApplicationController
 
     private
         def client_params
-            params.permit(:name, :geo_location, :start_date, :end_date)
+            params.permit(:name, :weather_data, :geo_location, :start_date, :end_date)
             # { POST
             #     "name": "Kamau John",
-            #     "geo_location": "-1.0916910437520184, 37.01157644647871",
+            #     "geo_location": "-1.0916910437520184,37.01157644647871", !!!! DO not put a space between the lat & long. Separate with comma only
             #     "start_date": "2021-10-06",
             #     "end_date": "2021-10-08"
             # }
--- app/helpers/weather_call_helper.rb
+++ app/helpers/weather_call_helper.rb
@@ -1,20 +1,35 @@
-# require 'open-uri'
-# require 'net/http'
+require 'open-uri'
+require 'net/http'
 
 module WeatherCallHelper
-    include HTTParty
+    # include HTTParty
 
-    @API_KEY = '4e8f3c3664c51137dfbf9590317c4d42'
+    @API_KEY = 'ce3a3d9423215ed4e8ca858aa94a7941'
     # @weather_api = "api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lon}&appid=#{@API_KEY}" #open weather API
     def get_weather(lat, long)
-        # uri = URI("api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{long}&appid=#{@API_KEY}")
-        # res = Net::HTTP.get_response(uri)
-
-        res = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{long}&appid=#{@API_KEY}")
+        lat = lat.to_s[0..5]
+        long = long.to_s[0..5] 
+        url = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid=ce3a3d9423215ed4e8ca858aa94a7941&units=metric".to_s
+        res = HTTParty.get(url)
+        # res = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?lat=-1.09&lon=37.01&appid=ce3a3d9423215ed4e8ca858aa94a7941")
         if res.code == 200
-            return true, res.body
+            res = res.parsed_response
+            puts "STH::: #{res["weather"][0]["main"]}"
+               
+            content = Hash.new
+            content[:Location] = res["name"]
+            content[:Main] = res["weather"][0]["main"]
+            content[:Desc] = res["weather"][0]["description"]
+            content[:Temp_Avg] = res["main"]["temp"]
+            content[:Temp_Max] = res["main"]["temp_max"]
+            content[:Temp_Min] = res["main"]["temp_min"]
+            
+            content[:Rain] = res["rain"]["1h"] if res.include?("rain")
+            return true, content, url
         else
-            return false, res.code
+            return false, res.code, url
         end
     end
 end
+
+# http://api.openweathermap.org/data/2.5/weather?lat=-1.091&lon=37.011&appid=ce3a3d9423215ed4e8ca858aa94a7941&units=metric
