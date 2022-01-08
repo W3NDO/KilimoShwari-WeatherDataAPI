e283d88ba614b4429263989feaccedb8844b2827
W3NDO <wendonynag@gmail.com>
2022-01-08 13:56:18 +0300

    Got it

--- db/migrate/20220108100232_change_date_to_int_type.rb
+++ db/migrate/20220108100232_change_date_to_int_type.rb
@@ -1,18 +1,18 @@
 class ChangeDateToIntType < ActiveRecord::Migration[6.1]
   def change
-    remove_column :policies, :start_date
-    remove_column :policies, :end_date
-    add_column :policies, :start_date, :integer
-    add_column :policies, :end_date, :integer
+    # remove_column :policies, :start_date
+    # remove_column :policies, :end_date
+    # add_column :policies, :start_date, :integer
+    # add_column :policies, :end_date, :integer
 
-    remove_column :contracts, :start_date
-    remove_column :contracts, :end_date
-    add_column    :contracts, :start_date, :integer
-    add_column    :contracts, :end_date, :integer
+    # remove_column :contracts, :start_date
+    # remove_column :contracts, :end_date
+    # add_column    :contracts, :start_date, :integer
+    # add_column    :contracts, :end_date, :integer
 
-    remove_column :client_weather_data, :start_date
-    remove_column :client_weather_data, :end_date
-    add_column    :client_weather_data, :start_date, :integer
-    add_column    :client_weather_data, :end_date, :integer
+    # remove_column :client_weather_data, :start_date
+    # remove_column :client_weather_data, :end_date
+    # add_column    :client_weather_data, :start_date, :integer
+    # add_column    :client_weather_data, :end_date, :integer
   end
 end
