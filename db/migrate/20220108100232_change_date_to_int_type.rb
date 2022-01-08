a0c55bb5d0f06d7fbc4f98395c506065a192d63e
W3NDO <wendonynag@gmail.com>
2022-01-08 13:48:59 +0300

    Got it

--- db/migrate/20220108100232_change_date_to_int_type.rb
+++ db/migrate/20220108100232_change_date_to_int_type.rb
@@ -4,7 +4,7 @@ class ChangeDateToIntType < ActiveRecord::Migration[6.1]
       t.remove_column :start_date
       t.remove_column :end_date
     end
-    add_column :policies, :start_date :integer
-    add_column :policies, :end_date :integer
+    add_column :policies, :start_date, :integer
+    add_column :policies, :end_date, :integer
   end
 end
